524107b085495536b53dd7ed7962464cc2b7f27c
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2023-04-11 16:57:33 +0700

    🟢 Fiverr Fixes added

--- app/page.jsx
+++ app/page.jsx
@@ -1,28 +1,16 @@
+import Products from "./list/Products";
+
 const wp_api_endpoint = "https://bubblybeaks.com/wp-json/api/lists"; // this works on
 
 async function getData() {
   const res = await fetch(wp_api_endpoint);
-
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
   return res.json();
 }
 
-export default async function Page() {
+const ProductsPage = async () => {
   const products = await getData();
 
-  return (
-    <main className="main">
-      <h1>Home</h1>
-      {products.map((product) => (
-        <article key={product.id} className="product">
-          <h6 className="product-title">{product.title}</h6>
-        </article>
-      ))}
-    </main>
-  );
-}
+  return <Products products={products} />;
+};
+
+export default ProductsPage;
