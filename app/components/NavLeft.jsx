7fcd3a47b6690fbbafcc9fb0c4a25df53bed58c4
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2023-04-12 13:28:34 +0700

    🟢 Modals Work in Dev

--- app/components/NavLeft.jsx
+++ app/components/NavLeft.jsx
@@ -151,23 +151,29 @@ const NavLeft = () => {
             isOpen={modalIsOpen["modal-1"]}
             handleCloseModal={() => handleCloseModal("modal-1")}
           >
-            <div>
-              <button onClick={() => toggleModal("modal-4")}>
-                Open Modal 4
-              </button>
+            <h2>Modal 1</h2>
+            <p>Modal 1 contents go here.</p>
+            <button onClick={() => toggleModal("modal-3")}>Open Modal 3</button>
+          </Modal>
 
-              <Modal
-                isOpen={modalIsOpen["modal-4"]}
-                handleCloseModal={() => handleCloseModal("modal-4")}
-              >
-                <h2>Modal 4</h2>
-                <p>Modal 4 contents go here.</p>
-              </Modal>
+          <Modal
+            isOpen={modalIsOpen["modal-2"]}
+            handleCloseModal={() => handleCloseModal("modal-2")}
+          >
+            <h2>Modal 2</h2>
+            <p>Modal 2 contents go here.</p>
+          </Modal>
 
-              <button onClick={() => toggleModal("modal-2")}>
-                Open Modal 2
-              </button>
-            </div>
+          <Modal
+            isOpen={modalIsOpen["modal-3"]}
+            handleCloseModal={() => handleCloseModal("modal-3")}
+          >
+            <h2>Modal 3</h2>
+            <p>Modal 3 contents go here.</p>
+
+            <button onClick={() => closeModals(["modal-1", "modal-3"])}>
+              Close Modals 1 and 3
+            </button>
           </Modal>
         </ul>
       </nav>
--- app/modal/page.jsx
+++ app/modal/page.jsx
@@ -3,50 +3,47 @@
 import React, { useState } from "react";
 import Modal from "../components/Modal";
 import { useModal } from "../components/ModalUse";
-import { ModalProvider } from "../components/ModalContext";
 
-const TestPage = () => {
+const ModalPage = () => {
   // useModal component contains modal functionality
   const { modalIsOpen, toggleModal, handleCloseModal, closeModals } =
     useModal();
 
   return (
-    <ModalProvider>
-      <div>
-        <button onClick={() => toggleModal("modal-1")}>Open Modal 1</button>
-        <button onClick={() => toggleModal("modal-2")}>Open Modal 2</button>
+    <div>
+      <button onClick={() => toggleModal("modal-1")}>Open Modal 1</button>
+      <button onClick={() => toggleModal("modal-2")}>Open Modal 2</button>
 
-        <Modal
-          isOpen={modalIsOpen["modal-1"]}
-          handleCloseModal={() => handleCloseModal("modal-1")}
-        >
-          <h2>Modal 1</h2>
-          <p>Modal 1 contents go here.</p>
-          <button onClick={() => toggleModal("modal-3")}>Open Modal 3</button>
-        </Modal>
+      <Modal
+        isOpen={modalIsOpen["modal-1"]}
+        handleCloseModal={() => handleCloseModal("modal-1")}
+      >
+        <h2>Modal 1</h2>
+        <p>Modal 1 contents go here.</p>
+        <button onClick={() => toggleModal("modal-3")}>Open Modal 3</button>
+      </Modal>
 
-        <Modal
-          isOpen={modalIsOpen["modal-2"]}
-          handleCloseModal={() => handleCloseModal("modal-2")}
-        >
-          <h2>Modal 2</h2>
-          <p>Modal 2 contents go here.</p>
-        </Modal>
+      <Modal
+        isOpen={modalIsOpen["modal-2"]}
+        handleCloseModal={() => handleCloseModal("modal-2")}
+      >
+        <h2>Modal 2</h2>
+        <p>Modal 2 contents go here.</p>
+      </Modal>
 
-        <Modal
-          isOpen={modalIsOpen["modal-3"]}
-          handleCloseModal={() => handleCloseModal("modal-3")}
-        >
-          <h2>Modal 3</h2>
-          <p>Modal 3 contents go here.</p>
+      <Modal
+        isOpen={modalIsOpen["modal-3"]}
+        handleCloseModal={() => handleCloseModal("modal-3")}
+      >
+        <h2>Modal 3</h2>
+        <p>Modal 3 contents go here.</p>
 
-          <button onClick={() => closeModals(["modal-1", "modal-3"])}>
-            Close Modals 1 and 3
-          </button>
-        </Modal>
-      </div>
-    </ModalProvider>
+        <button onClick={() => closeModals(["modal-1", "modal-3"])}>
+          Close Modals 1 and 3
+        </button>
+      </Modal>
+    </div>
   );
 };
 
-export default Page;
+export default ModalPage;
